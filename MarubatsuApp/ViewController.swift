ec0b2e3a5a1c92b1d7eeca3e0151437cbf1347f9
YASUO0715 <yasuo0715niihori@gmail.com>
2021-11-06 08:49:17 +0900

    Initial Commit

---
+++ MarubatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarubatsuApp
+//
+//  Created by Yasuo Niihori on 2021/11/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
